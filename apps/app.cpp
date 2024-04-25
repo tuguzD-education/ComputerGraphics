f1133d7643b3c155d3c3343be416bce1f4a622d3
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2024-04-25 15:31:49 +0300

    Include libraries in a better way

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,11 +1,6 @@
 #include "computer_graphics/game.hpp"
 #include "computer_graphics/window.hpp"
 
-#pragma comment(lib, "d3d11.lib")
-#pragma comment(lib, "dxgi.lib")
-#pragma comment(lib, "d3dcompiler.lib")
-#pragma comment(lib, "dxguid.lib")
-
 int main() {
     computer_graphics::Window window{
         "Application",
